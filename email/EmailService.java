2f6f46074656d5894d12eaa3bd978a7dc162b7e4
aopaixao <aopaixao@outlook.com>
2024-05-16 14:20:06 -0300

    Create EmailService.java

---
+++ email/EmailService.java
@@ -0,0 +1,22 @@
+@Service
+public class EmailService {
+	@Autowired
+	public JavaMailSender emailSender;
+	
+	public void enviarEmail(String destinatario, String assunto, String mensagem) {
+		var mailMessage = new SimpleMailMessage();
+		
+		mailMessage.setTo(destinatario);
+		mailMessage.setSubject(assunto);
+		mailMessage.setText(mensagem);
+		mailMessage.setFrom("aopaixao@outlook.com");
+		
+		try {
+			emailSender.send(mailMessage);
+		}catch(Exception ex) {
+			System.out.println("Ocorreu um erro ao tentar enviar o e-mail: " 
+					+ ex.getMessage());
+		}
+		
+	}
+}
